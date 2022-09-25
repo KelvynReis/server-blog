cb160e61b99d05bd69221db3c6c0c3d7813b7d90
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-09-25 18:22:44 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', ''),
+      port: env.int('DATABASE_PORT', ''),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+    pool : {
+      min: 0,
+      max: 5,
+    },
+  },
+});
